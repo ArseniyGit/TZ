2f10c1b4f3abbd77cd113f71a1bfb72da9ddf72a
ArseniyGit <fedya.arseniy@mail.ru>
2025-06-25 14:02:00 +0300

    config done

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"os"
+	"time"
+)
+
+type Config struct {
+	Port           string
+	ReadTimeout    time.Duration
+	WriteTimeout   time.Duration
+	RequestTimeout time.Duration
+}
+
+func Load() *Config {
+	return &Config{
+		Port:           getEnv("PORT", "8080"),
+		ReadTimeout:    getDurationEnv("READ_TIMEOUT", 10*time.Second),
+		WriteTimeout:   getDurationEnv("WRITE_TIMEOUT", 10*time.Second),
+		RequestTimeout: getDurationEnv("REQUEST_TIMEOUT", 30*time.Second),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getDurationEnv(key string, defaultValue time.Duration) time.Duration {
+	if value := os.Getenv(key); value != "" {
+		if duration, err := time.ParseDuration(value); err == nil {
+			return duration
+		}
+	}
+	return defaultValue
+}
